ec6847459fdfab8cb81059f3b152b22a38664d88
Delos-343 <mfachry.handoko@gmail.com>
2024-05-21 21:01:37 +0700

    Changed style for readability

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
     surface: Colors.grey.shade300,
-    primary: Colors.grey.shade700,
+    primary: Colors.grey.shade800,
     secondary: Colors.grey.shade200,
     tertiary: Colors.white,
     inversePrimary: Colors.grey.shade900,
