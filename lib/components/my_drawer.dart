e53d818086a831cb8b6c12645fdf5cad9062dfdd
Delos-343 <mfachry.handoko@gmail.com>
2024-06-05 14:44:34 +0700

    Added navigation to route for Settings

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -8,41 +8,57 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // Logo
-          DrawerHeader(
-            child: Center(
-              child: Icon(
-                Icons.message,
-                color: Theme.of(context).colorScheme.primary,
-                size: 40,
+          Column(
+            children: [
+              // Logo
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.message,
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 40,
+                  ),
+                ),
               ),
-            ),
-          ),
+              // Home
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("H o m e"),
+                  leading: Icon(Icons.home_outlined),
+                  onTap: () {
+                    // Pop the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
 
-          // Home
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: ListTile(
-              title: Text("H o me "),
-              leading: Icon(Icons.home_outlined),
-              onTap: () {},
-            ),
-          ),
-
-          // Settings
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: ListTile(
-              title: Text("S e t t i n g s"),
-              leading: Icon(Icons.settings_outlined),
-              onTap: () {},
-            ),
+              // Settings
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  title: Text("S e t t i n g s"),
+                  leading: Icon(Icons.settings_outlined),
+                  onTap: () {
+                    // Pop the drawer
+                    Navigator.pop(context);
+                    // Navigate to Settings
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SettingsPage(),
+                        ));
+                  },
+                ),
+              ),
+            ],
           ),
 
           // Logout
           Padding(
-            padding: const EdgeInsets.only(left: 25.0),
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25),
             child: ListTile(
               title: Text("L o g o u t"),
               leading: Icon(Icons.logout_outlined),
