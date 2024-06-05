9b44da247fffb93c312bbb4a82fcf7609ab63d95
Delos-343 <mfachry.handoko@gmail.com>
2024-06-05 14:50:02 +0700

    Updated titles for list

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -27,7 +27,7 @@ class MyDrawer extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 25.0),
                 child: ListTile(
-                  title: Text("H o m e"),
+                  title: Text("H O M E"),
                   leading: Icon(Icons.home_outlined),
                   onTap: () {
                     // Pop the drawer
@@ -40,7 +40,7 @@ class MyDrawer extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 25.0),
                 child: ListTile(
-                  title: Text("S e t t i n g s"),
+                  title: Text("S E T T I N G S"),
                   leading: Icon(Icons.settings_outlined),
                   onTap: () {
                     // Pop the drawer
@@ -61,7 +61,7 @@ class MyDrawer extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(left: 25.0, bottom: 25),
             child: ListTile(
-              title: Text("L o g o u t"),
+              title: Text("L O G   O U T"),
               leading: Icon(Icons.logout_outlined),
               onTap: () {},
             ),
