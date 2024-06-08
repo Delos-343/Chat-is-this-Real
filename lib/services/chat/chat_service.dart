e576eb1be77a5715c7be109d485da5b18c227197
Delos-343 <mfachry.handoko@gmail.com>
2024-06-08 10:38:12 +0700

    Added chat service to get users

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,7 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatService {
   // get inst of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // get user stream
+  /*
+    List<Map<String, dynamic> = 
+    [
+      {
+        'email': oogabooga@gmail.com,
+        'id': ...
+      }
+      {
+        'email': grog@gmail.com,
+        'id': ...
+      }
+    ]
+  */
+  Stream<List<Map<String, dynamic>>> getUserStream() {
+    return _firestore.collection("Users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        // for each user
+        final user = doc.data();
+
+        // get user
+        return user;
+      }).toList();
+    });
+  }
 
   // send msg
 
