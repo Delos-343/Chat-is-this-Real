4abc417e9d2d6fcb83e6cbb7ce1d78775b0228e7
Delos-343 <mfachry.handoko@gmail.com>
2024-06-09 15:08:20 +0700

    Set up logic for input field for messages

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_is_this_real_app/services/auth/auth_service.dart';
+import 'package:chat_is_this_real_app/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -7,6 +9,13 @@ class ChatPage extends StatelessWidget {
     super.key,
     required this.receiverEmail,
   });
+
+  // Text controller for messages
+  final TextEditingController _messageController = TextEditingController();
+
+  // Chat + Auth services
+  final ChatService _chatService = ChatService();
+  final AuthService _authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
