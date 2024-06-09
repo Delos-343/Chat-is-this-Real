253e6f89719ab2d98efc9cf51ee6b67115de18b7
Delos-343 <mfachry.handoko@gmail.com>
2024-06-09 16:38:44 +0700

    Increased contrast color for receiver chat bubble

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -14,7 +14,7 @@ class ChatBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: isCurrentUser ? Colors.lightBlue : Colors.grey.shade400,
+        color: isCurrentUser ? Colors.lightBlue : Colors.grey.shade500,
         borderRadius: BorderRadius.circular(15),
       ),
       padding: const EdgeInsets.all(18),
