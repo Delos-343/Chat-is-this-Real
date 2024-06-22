f071dc28f24b460db647213e14ee2fa5990cf6f3
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2024-06-23 04:34:31 +0700

    Stashed firebase_app_check

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:chat_is_this_real_app/services/auth/auth_gate.dart';
 import 'package:chat_is_this_real_app/firebase_options.dart';
 import 'package:chat_is_this_real_app/themes/theme_provider.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,11 +8,6 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await FirebaseAppCheck.instance.activate(
-    webRecaptchaSiteKey: 'YOUR_RECAPTCHA_SITE_KEY',
-    androidPackageName: 'com.example.chat_is_this_real_app',
-    androidSafetyNetApiKey: 'YOUR_SAFETYNET_API_KEY',
-  );
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
